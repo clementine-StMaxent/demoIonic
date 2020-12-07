7744d3cbc70b4b6c0b0b2aba3eccbb634c85f53f
clémentine-StMaxent <clementine.saintmaxent@gmail.com>
2020-12-07 14:39:04 +0100

    etudiant

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -20,23 +20,31 @@ export class HomePage {
     { id: 2, name: 'Noémie', grade: 18, isProjectDone: true, comment: 'Brillante' },
     { id: 3, name: 'Umberto', grade: 6.5, isProjectDone: false, comment: 'Dissipé, perturbateur' }
   ];
-
+  numFastStudent: number;
   message: string = '';
 
-  constructor() { }
+  constructor() {
+    this.numFastStudent = this.getStudentProjectFinish().length;
+  }
 
   onClick(index: number) {
     this.message = this.students[index].comment;
   }
 
   // calback de la propriété onChange.
-  onChange(e: any) {
+  onChange(e: any, index: number) {
     // target est une clé ayant pour valeur l'élement écouteur.
-    console.log(e.target.checked);
+    // console.log(e.target.checked);
+
+    // MAJ de la liste de données.
+    let checked = e.target.checked;
+    this.students[index].isProjectDone = checked;
+    console.log(this.students);
   }
 
   // Retourne les étudiants ayant terminer leur projet.
-  getStudentProjectFinish(){
-
+  getStudentProjectFinish(): Student[] {
+    // le filter permet de push un étudiant dans le tableau les étudiants dont la propriété isProjectDone === true.
+    return this.students.filter(student => student.isProjectDone === true);
   }
 }
